d8ce741c464fba66944a0ada137644f3155d0e34
Gabriel Castro <buguimo321@outlook.com>
2024-02-19 15:23:29 -0300

    matplot first try

--- bot_twitter.py
+++ bot_twitter.py
@@ -1,6 +1,13 @@
 import tweepy
 from textblob import TextBlob as tb
+import matplotlib.pyplot as plt
 
+
+def showGraph(polarities, subjectivity, xAxis):
+    plt.title("Polarity")
+    plt.plot(xAxis, polarities, '-', color='red')
+    plt.plot(xAxis, subjectivity, '-', color='blue')
+    plt.show()
 """
 consumer_key = 'key q tenho q colocar'
 consumer_secret = 'segredinho rsrs'
@@ -12,13 +19,20 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 """
-public_tweets = api.search(q='python', count=100)
+#public_tweets = api.search(q='python', count=100)
+public_tweets = ["Fuck you", "I hate you", "Sorry"]
+
+
+xAxis = range(len(public_tweets))
 
 polarities = []
+subject = []
 for tweet in public_tweets:
-    analysis = tb(tweet.text)
+    analysis = tb(tweet)
     polarities.append(analysis.sentiment.polarity)
+    subject.append(analysis.sentiment[1])
 
+showGraph(polarities, subject, xAxis)
 average_polarity = sum(polarities) / len(polarities)
 print("Média de sentimento: ", average_polarity)
 
