5b63bf37dc8ae1ff53b5edaa2e8977dce13720c2
Gabriel Castro <buguimo321@outlook.com>
2024-02-24 19:23:55 -0300

    API call fix

--- bot_twitter.py
+++ bot_twitter.py
@@ -5,11 +5,15 @@ import matplotlib.pyplot as plt
 
 
 bearer_token = 'AAAAAAAAAAAAAAAAAAAAAL7NsQEAAAAAtm62c5x8bI1Mrc0oHuCr2Z5C3Pw%3DMipCUX0LSppU2w5XQPhHrn3bnIFa2GJ4OKrW0ZDtqCzChKyaRm'
+consumer_key = 'YBDraw0XQjcV4wNpYS8F5d2bs'
+consumer_secret = 'RqraaaqBealjE1iV8s1MUY8ciPyU1njfbEb4HvXHmiYLNt0N2C'
+access_token = '1759704638197039105-4AJDaU3HLPjKHbFfm4N2oSm3tSIkml'
+access_token_secret = 'OT9GHXOw7inrS9RWrInpHiPKbZ3AbplGAaQbry9Pm9Hhb'
 client = tweepy.Client(bearer_token=bearer_token, 
-                       consumer_key='YBDraw0XQjcV4wNpYS8F5d2bs', 
-                       consumer_secret='RqraaaqBealjE1iV8s1MUY8ciPyU1njfbEb4HvXHmiYLNt0N2C',
-                       access_token='1759704638197039105-4AJDaU3HLPjKHbFfm4N2oSm3tSIkml',
-                       access_token_secret='OT9GHXOw7inrS9RWrInpHiPKbZ3AbplGAaQbry9Pm9Hhb'
+                       consumer_key=consumer_key, 
+                       consumer_secret=consumer_secret,
+                       access_token= access_token,
+                       access_token_secret=access_token_secret
                        )
 
 #tweets = [tweet['text'] for tweet in response_data['data']]
@@ -22,15 +26,6 @@ for tweet_text in tweets:
     analysis = tb(tweet_text)
     polarities.append(analysis.sentiment.polarity)
 
-consumer_key = 'YBDraw0XQjcV4wNpYS8F5d2bs'
-consumer_secret = 'RqraaaqBealjE1iV8s1MUY8ciPyU1njfbEb4HvXHmiYLNt0N2C'
-access_token = '1759704638197039105-4AJDaU3HLPjKHbFfm4N2oSm3tSIkml'
-access_token_secret = 'OT9GHXOw7inrS9RWrInpHiPKbZ3AbplGAaQbry9Pm9Hhb'
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
 
 average_polarity = sum(polarities) / len(polarities)
 client.create_tweet(text="test")
